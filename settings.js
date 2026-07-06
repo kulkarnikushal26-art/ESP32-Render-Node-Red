a964aca3d1cfc69d0e260d09f8584e4f4c28ea2a
kulkarnikushal26-art <kulkarnikushal26@gmail.com>
2026-07-06 21:50:41 +0530

    settings.js

---
+++ settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+    uiPort: process.env.PORT || 1880,
+    mqttReconnectTime: 15000,
+    serialReconnectTime: 15000,
+    debugMaxLength: 1000,
+    flowFile: 'flows.json',
+    functionGlobalContext: {},
+    exportGlobalContextKeys: false,
+    logging: {
+        console: {
+            level: "info",
+            metrics: false,
+            audit: false
+        }
+    },
+    adminAuth: null
+};
