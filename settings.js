0fe965235927770c43657d07fa2917421489a25b
kulkarnikushal26-art <kulkarnikushal26@gmail.com>
2026-07-07 20:08:06 +0530

    settings.js

--- settings.js
+++ settings.js
@@ -4,19 +4,7 @@ module.exports = {
     serialReconnectTime: 15000,
     debugMaxLength: 1000,
     flowFile: 'flows.json',
-    
-    // ALLOW FUNCTION NODES TO USE THE MONGODB DRIVER NATIVELY
-    functionGlobalContext: {
-        mongodb: require('mongodb')
-    },
-    
     exportGlobalContextKeys: false,
-    logging: {
-        console: {
-            level: "info",
-            metrics: false,
-            audit: false
-        }
-    },
+    logging: { console: { level: "info", metrics: false, audit: false } },
     adminAuth: null
 };
